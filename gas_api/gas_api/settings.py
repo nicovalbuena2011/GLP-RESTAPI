d4f97d7dc4ce863b6c8988496e6204afda3937f0
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2024-02-18 09:29:33 -0500

    swagger documentation

--- gas_api/gas_api/settings.py
+++ gas_api/gas_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
     'sales',
     'customers',
     'employes',
@@ -127,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
