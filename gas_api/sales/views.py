34111cfeb6676f858ba99d9d4f5e021db541abf8
nicovalbuena2011 <nicovalbuena2011@gmail.com>
2024-02-21 18:37:57 -0500

    Filtra por fecha metodo de pago

--- gas_api/sales/views.py
+++ gas_api/sales/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Ventas
+from django.db.models import Q
 from rest_framework import viewsets, authentication, permissions
 from rest_framework.views import APIView
 from rest_framework.exceptions import ValidationError
@@ -20,17 +21,29 @@ class VentasViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return serializers.VentasSerializer
         return serializers.CreateUpdateVentasSerializer
-
+    
+@extend_schema(tags=["Ventas"])
 class VentasPorPeriodoDeTiempoAPI(APIView):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     def get(self, request, *args, **kwargs):
-        fecha_inicio = self.request.query_params.get('fecha_inicio')
-        fecha_fin = self.request.query_params.get('fecha_fin')
-        # print(f'Fecha de inicio: {fecha_inicio}')
-        # print(f'Fecha de fin: {fecha_fin}')
-        # Filtrar las ventas por las fechas proporcionadas
-        ventas = Ventas.objects.filter(created_at__range=[fecha_inicio, fecha_fin])
+        fecha_inicio = request.query_params.get('fecha_inicio')
+        fecha_fin = request.query_params.get('fecha_fin')
+        metodo_pago = request.query_params.get('metodo_pago')
+        
+        # Inicializar una consulta base que devuelve todas las ventas
+        query = Q()
+        
+        # Agregar filtro por fechas si están presentes
+        if fecha_inicio and fecha_fin:
+            query &= Q(created_at__range=[fecha_inicio, fecha_fin])
+        
+        # Agregar filtro por método de pago si está presente
+        if metodo_pago:
+            query &= Q(metodo_pago=metodo_pago)
+        
+        # Filtrar las ventas según la consulta construida
+        ventas = Ventas.objects.filter(query)
         
         # Serializar los datos filtrados
         serializer = serializers.VentasSerializer(ventas, many=True)
